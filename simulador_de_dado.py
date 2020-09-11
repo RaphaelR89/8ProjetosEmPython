a1ae21297f727aa97019937dc4f72e8c2938f954
Raphael <66534805+RaphaelR89@users.noreply.github.com>
2020-09-11 20:38:49 -0300

    layout pro jogo com PySimpleGUI

--- simulador_de_dado.py
+++ simulador_de_dado.py
@@ -1,7 +1,7 @@
 # Simulador de dado
 # Simular o uso de um dado, gerando um valor de 1 até 6
 import random
-import PySimpleGUI
+import PySimpleGUI as sg
 
 
 class SimuladorDeDado:
@@ -9,18 +9,29 @@ class SimuladorDeDado:
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem = "Você gostaria de gerar um novo valor para o dado?"
+        #Criar uma tela de visualização para o jogo com PySimpleGUI
+        # Layout
+        self.layout = [
+            [sg.Text('Jogar o dado?')],
+            [sg.Button('sim'),sg.Button('não')]
+        ]
 
     def Iniciar(self):
-        resposta = input(self.mensagem)
+         # criar uma janela
+        janela = sg.Window('Simulador de Dado',layout=self.layout)
+        # ler os valores da tela
+        self.eventos, self.valores = janela.Read()
+        # fazer alguma coisa com esses valores
+        
         try:
-            if resposta == 'sim' or resposta == 's':
+            if self.eventos == 'sim' or self.eventos == 's':
                 self.GerarValorDodado()
-            elif resposta == 'não' or resposta == "n":
+            elif self.eventos == 'não' or self.eventos == "n":
                 print('Agradecemos sua participação!')
             else:
                 print('Favor digitar sim ou não...')
         except:
-            print("Ocorreu um erro ao receber sua resposta...")
+                print("Ocorreu um erro ao receber sua resposta...")
 
     def GerarValorDodado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo))
