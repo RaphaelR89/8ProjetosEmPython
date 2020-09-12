7598bc0751b1664121b1f6706e2909fceaedf3d8
Raphael <66534805+RaphaelR89@users.noreply.github.com>
2020-09-11 21:56:10 -0300

    jogo completado

--- Chute_o_numero.py
+++ Chute_o_numero.py
@@ -8,19 +8,31 @@ class ChuteONumero:
         self.valor_aleatorio = 0
         self.valor_minimo = 1
         self.valor_maximo = 100
+        self.tentar_novamente = True
 
     def Iniciar(self):
         self.GerarNumeroAleatorio()
         self.PedirValorAleatorio()
-        if int(self.valor_do_chute) > self.valor_aleatorio:
-            print('Chute um valor mais baixo!')
-            self.PedirValorAleatorio()
-        elif int(self.valor_do_chute) < self.valor_aleatorio:
-            print('Chute um valor mais alto!')
-            self.PedirValorAleatorio()
-
+        try:
+            while self.tentar_novamente == True:
+                if int(self.valor_do_chute) > self.valor_aleatorio:
+                    print('Chute um valor mais baixo!')
+                    self.PedirValorAleatorio()
+                elif int(self.valor_do_chute) < self.valor_aleatorio:
+                    print('Chute um valor mais alto!')
+                    self.PedirValorAleatorio()
+                if int(self.valor_do_chute) == self.valor_aleatorio:
+                    self.tentar_novamente = False
+                    print('Parabéns, você acertou!')
+        except:
+            print('Favor digitar apenas números inteiros!')
+            self.Iniciar()
     def PedirValorAleatorio(self):
         self.valor_do_chute = input('Chute um número: ')
 
     def GerarNumeroAleatorio(self):
        self.valor_aleatorio = random.randint(self.valor_minimo, self.valor_maximo)
+
+
+chute = ChuteONumero()
+chute.Iniciar()
