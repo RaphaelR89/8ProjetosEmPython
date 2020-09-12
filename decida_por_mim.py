47cc9f93f0c726a7d015fa2df708b56632a6dd94
Raphael <66534805+RaphaelR89@users.noreply.github.com>
2020-09-11 23:18:37 -0300

    completando gui do jogo

--- decida_por_mim.py
+++ decida_por_mim.py
@@ -1,6 +1,7 @@
 #Faça uma pergunta para o programa e ele terá q te dar uma resposta
 
 import random
+import PySimpleGUI as sg
 
 class DecidaPorMim:
     def __init__(self):
@@ -12,8 +13,17 @@ class DecidaPorMim:
         ]
 
     def Iniciar(self):
-        input("Faça sua pergunta: ")
-        print(random.choice(self.respostas))
+        #layout
+        layout = [[sg.Text('Faça sua pergunta: ')],[sg.Input()],[sg.Output(size=(20,10))],[sg.Button('Decida por mim')]]
+        #criar janela
+        self.janela = sg.Window('Decida por mim!', layout=layout)
+        while True:
+            #ler os valores
+            self.eventos, self.valores = self.janela.Read()
+            #fazer algo com os valores
+            if self.eventos == 'Decida por mim':
+                print(random.choice(self.respostas))
+                
 
 decida = DecidaPorMim()
 decida.Iniciar()
